1a7ed564d50e6fd1ec5c99d0e1b49c7aca296929
novakhenrique <novak.henrique@escola.pr.gov.br>
2026-03-31 07:50:02 -0300

    Create desafio3.py

---
+++ desafio3.py
@@ -0,0 +1,7 @@
+# Criando uma lista com 5 nomes
+nomes = ["Ana", "Carlos", "Maria", "João", "Fernanda"]
+
+# Imprimindo cada nome da lista usando um laço de repetição (for)
+print("Lista de nomes:")
+for nome in nomes:
+    print(nome)
